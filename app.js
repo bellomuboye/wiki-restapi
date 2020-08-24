5087e6d00a044938e181d6326e26eee1b16bbc4c
bellomuboye <crystalbell.omuboye@gmail.com>
2020-08-24 23:33:26 +0100

    PATCH specific article

--- app.js
+++ app.js
@@ -89,6 +89,19 @@ app.route('/articles/:specificArticle')
 				}
 			}
 		);
+	})
+	.patch(function (req, res) {
+		Article.update(
+			{title: req.params.specificArticle},
+			{$set: req.body},
+			function (err) {
+				if (!err) {
+					res.send("Successfully updated article");
+				} else {
+					res.send(err);
+				}
+			}
+		);
 	});
 
 app.listen(3000, function() {
