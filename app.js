78c58e5696dd2cca115124d340b2b5f6e0ee8239
bellomuboye <crystalbell.omuboye@gmail.com>
2020-08-24 22:20:51 +0100

    APIs for all articles

--- app.js
+++ app.js
@@ -24,6 +24,45 @@ const articlesSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articlesSchema);
 
+app.get('/articles', function (req, res) {
+	Article.find({}, function (err, allArticles) {
+		if (!err) {
+			res.send(allArticles);
+		} else {
+			res.send(err);
+		}
+	});
+});
+
+
+app.post('/articles', function (req, res) {
+	const newArticle = new Article ({
+		title: req.body.title,
+		content: req.body.content
+	});
+
+	newArticle.save(function (err) {
+		if (!err) {
+			res.send("Successfully added a new article " + req.body.title);
+		} else {
+			res.send(err);
+		}
+	});
+
+});
+
+app.delete('/articles', function (req, res) {
+	Article.deleteMany({}, function (err) {
+		if (!err) {
+			res.send("Successfully deleted all articles")
+		} else {
+			res.send(err);
+		}
+	});
+});
+
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
