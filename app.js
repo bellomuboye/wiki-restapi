9277e0ebcc56822e0018cb257442adb31e7f51be
bellomuboye <crystalbell.omuboye@gmail.com>
2020-08-24 22:40:29 +0100

    refactor code to use chained route handlers

--- app.js
+++ app.js
@@ -24,42 +24,40 @@ const articlesSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articlesSchema);
 
-app.get('/articles', function (req, res) {
-	Article.find({}, function (err, allArticles) {
-		if (!err) {
-			res.send(allArticles);
-		} else {
-			res.send(err);
-		}
+app.route('articles')
+	.get(function (req, res) {
+		Article.find({}, function (err, allArticles) {
+			if (!err) {
+				res.send(allArticles);
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.post(function (req, res) {
+		const newArticle = new Article ({
+			title: req.body.title,
+			content: req.body.content
+		});
+
+		newArticle.save(function (err) {
+			if (!err) {
+				res.send("Successfully added a new article " + req.body.title);
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.delete (function (req, res) {
+		Article.deleteMany({}, function (err) {
+			if (!err) {
+				res.send("Successfully deleted all articles")
+			} else {
+				res.send(err);
+			}
+		});
 	});
-});
 
-
-app.post('/articles', function (req, res) {
-	const newArticle = new Article ({
-		title: req.body.title,
-		content: req.body.content
-	});
-
-	newArticle.save(function (err) {
-		if (!err) {
-			res.send("Successfully added a new article " + req.body.title);
-		} else {
-			res.send(err);
-		}
-	});
-
-});
-
-app.delete('/articles', function (req, res) {
-	Article.deleteMany({}, function (err) {
-		if (!err) {
-			res.send("Successfully deleted all articles")
-		} else {
-			res.send(err);
-		}
-	});
-});
 
 
 
